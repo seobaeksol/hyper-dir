7f957b0e39f27d60b749410f7d2adce01f6d5e82
seobaeksol <suyoung154@gmail.com>
2025-04-19 21:36:39 +0900

    Add custom scrollbar

--- src/components/layout/panel/PanelWrapper.tsx
+++ src/components/layout/panel/PanelWrapper.tsx
@@ -5,11 +5,11 @@ export const PanelWrapper = () => {
   const { panels, activePanelId } = usePanelStore();
 
   return (
-    <div className="flex flex-1">
+    <div className="flex flex-1 overflow-hidden">
       {panels.map((panel) => (
         <div
           key={panel.id}
-          className={`flex flex-col flex-1 ${
+          className={`flex flex-col flex-1 overflow-hidden ${
             panel.id === activePanelId ? "" : "hidden"
           }`}
         >
